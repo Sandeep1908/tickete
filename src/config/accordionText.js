9762450e3efc4553467ca09efd91b1e90eee718e
Sandeep <sandbro7163337@gmail.com>
2024-02-09 17:06:33 +0530

    add config for text

---
+++ src/config/accordionText.js
@@ -0,0 +1,29 @@
+const accordionData=[
+    {
+        title:'What should i do on my first trip to rome?',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?',
+        id:1
+    },
+    {
+        title:'What are some hidden gems to see in rome?',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?',
+        id:2
+    },
+    {
+        title:'How much time should i spend in rome?',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?',
+        id:3
+    },
+    {
+        title:'What food is rome known for?',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?',
+        id:4
+    },
+    {
+        title:'What is the best to get around rome?',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?consectetur, adipisicing elit. Esse ad, enim possimus ab ipsum reiciendis, quidem voluptas officiis libero id eaque vero ipsa odit nisi. Nulla dolorum odit iusto dolor?',
+        id:5
+    },
+]
+
+export default accordionData
